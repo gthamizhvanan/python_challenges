5c28bb091fce2b13a9e7f4ad4dcdf93f5a8617c9
gthamizhvanan <thamizhvanan.g@indiaproperty.com>
2016-01-29 11:43:39 +0530

    flames-app

---
+++ flames.py
@@ -0,0 +1,43 @@
+# FLAMES #
+fn = input('Enter First Name  :')
+ln = input('Enter Second Name :')
+
+fn = fn.upper().replace(' ','')
+ln = ln.upper().replace(' ','')
+#print (fn)
+def findDiff(fn,ln):
+ for i,j in enumerate(ln):
+  for p,q in enumerate(fn):
+   if j==q:
+    fn = fn.replace(j,'',1)
+    ln = ln.replace(j,'',1)
+    break
+ diffVal = len(fn)+len(ln)
+ return diffVal
+
+intDiffLen = findDiff(fn,ln)
+ch         = intDiffLen - 1
+
+def findRelationShip(ch):
+ l = ['F','L','A','M','E','S']
+ for i,j in enumerate(l):
+  if(len(l) == 1):
+   return l
+  else:
+   t = ch % len(l)
+   del l[t]
+   if(len(l) > 1):
+    alv = -(len(l)-t)
+    if len(l[alv:]) >= 1:
+     lv = l[alv:]
+     fv = l[:alv]
+     l = lv+fv
+   else:
+     return l
+
+lstFlames = {'F':'Friend','L':'Love','A':'Affection','M':'Marriage','E':'Enemy','S':'Sister'}
+lstRelFind = findRelationShip(ch)
+strRelFind = ''.join(lstRelFind)
+print (lstFlames[strRelFind])
+
+
